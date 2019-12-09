82f595ff12b4cc0d8519d14ee46fa2163129067b
Pavel Ulc <p.ulc@seznam.cz>
2019-12-09 22:19:06 +0100

    Add project files.

---
+++ ZeiterfassungPierburg/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ZeiterfassungPierburg.MvcApplication" Language="C#" %>
---
+++ ZeiterfassungPierburg/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZeiterfassungPierburg.Startup))]
+namespace ZeiterfassungPierburg
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

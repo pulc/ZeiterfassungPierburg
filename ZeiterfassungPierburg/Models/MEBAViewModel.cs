bb6e7151900386d8b63aa483316a61d3c27df2b0
Pavel Ulc <p.ulc@seznam.cz>
2019-12-19 20:34:38 +0100

    add backup

---
+++ ZeiterfassungPierburg/Models/MEBAViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using ZeiterfassungPierburg.Data;
+
+namespace ZeiterfassungPierburg.Models
+{
+    public class MEBAViewModel 
+    {
+        public Fertigungsteil fertigungsteil;
+        public Produktionsanlage produktionsanlage;
+        public Mitarbeiter mitarbeiter;
+        public MitarbeiterInSchicht mitarbeiterInSchicht;
+        public SchichtInfo schichtInfo;
+    }
+}
+
